f467bf0f5add59fa3f3b3e0d80bcad911e4c7835
arpitasubhadarshini <subhadarshiniarpita40@gmail.com>
2024-09-11 12:15:31 +0000

    function

---
+++ basic-js/function.js
@@ -0,0 +1,29 @@
+// non-parameterised function
+// function sum() {
+//     let a =2;
+//     let b=3;
+//     let c=a+b;
+//     console.log(c);
+// }
+// sum();
+// parameterised function
+// function sum(x,y) {
+//     let c=x+y;
+//     console.log(c);
+// }
+// sum(2,8);
+// sum(1,4);
+
+// function square() {
+//     let a=4;
+//     let b = a*a;
+//     console.log(b);
+// }
+// square()
+
+function square(x) {
+     let s=x*x;
+     console.log(s);
+}
+square(3);
+square(4);
